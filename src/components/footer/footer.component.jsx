832e21887506ad50d3435b27599f8b2f6f8380d9
Mark Phillips <mphill05@gmail.com>
2021-03-29 17:06:03 -0700

    recent updates

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,17 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import "./footer.styles.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import logo from "../Assets/logo.png";
+import { faEnvelope, faHome, faProjectDiagram, faToolbox } from "@fortawesome/free-solid-svg-icons";
+
 
 import './footer.styles.scss';
 
-class Footer extends React.Component {
-    render() {
-        return (
-            <div className='footer' >
-                <ul className='social-container'>
-                    <p>&copy; 2021 by PhillStack Consulting, LLC.</p>
-                </ul>
-            </div>
-        )
-    };
+function Footer() {
+    const [expand, updateExpanded] = useState(false);
+    const [navColor, updateNav] = useState(false);
+
+    return (
+        <Navbar
+            fixed="bottom"
+            expand="md"
+            className={navColor ? "sticky" : "navbar"}
+        >
+            <Container>
+                <Navbar.Toggle
+                    aria-controls="responsive-navbar-nav"
+                    onClick={() => {
+                        updateExpanded(expand ? false : "expanded");
+                    }}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto" defaultActiveKey="#home">
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+                                <FontAwesomeIcon icon={faHome} />
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link
+                                as={Link}
+                                to="/services"
+                                onClick={() => updateExpanded(false)}
+                            >
+                                <FontAwesomeIcon icon={faToolbox} />
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link
+                                as={Link}
+                                to="/projects"
+                                onClick={() => updateExpanded(false)}
+                            >
+                                <FontAwesomeIcon icon={faProjectDiagram} />
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link
+                                as={Link}
+                                to="/contact"
+                                onClick={() => updateExpanded(false)}
+                            >
+                                <FontAwesomeIcon icon={faEnvelope} />
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default Footer;
