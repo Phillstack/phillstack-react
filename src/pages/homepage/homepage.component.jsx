b2ecc9cf6d48d8a64998c5230ae6e0167d416ca6
PhillStack <grant@phillstack.com>
2021-04-04 02:49:14 -0700

    WIP homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from "react-router-dom";
+import Glide from "@glidejs/glide"
 
 import './homepage.styles.scss';
 
-const HomePage = () => (
-    <div className='homeContainer'>
-        <div className='titleContainer'>
-            <strong>HomePage</strong>
-            <hr />
+const sliderConfiguration = {
+    type: 'slider',
+    perView: 3,
+    focusAt: 'center',
+    breakpoints: {
+        800: {
+            perView: 3
+        },
+        480: {
+            perView: 3
+        }
+    }
+};
+
+const HomePage = () => {
+    const glide = new Glide('.glide', sliderConfiguration);
+
+    useEffect(() => {
+        return () => glide.mount()
+    }, [glide])
+
+    return (
+        <div className='homeContainer'>
+            <div id='banner'>
+                <h1>PHILLSTACK</h1>
+            </div>
+            <div className="glide multi1">
+                <div className="glide__track" data-glide-el="track">
+                    <ul className="glide__slides">
+                        <li className="glide__slide">1</li>
+                        <li className="glide__slide">2</li>
+                        <li className="glide__slide">3</li>
+                        <li className="glide__slide">4</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+            <Link to="/contact" className="btn btn-primary">Connect</Link>
+            </div>
         </div>
-        <h1>COMING SOON!</h1>
-    </div>
-)
+    );
+};
 
 export default HomePage;
