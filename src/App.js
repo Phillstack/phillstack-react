f94af2f8d4b8bc0fd61d24aa4799fa3440661818
PhillStack <grant@phillstack.com>
2021-04-25 15:29:20 -0500

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import Particles from 'react-particles-js';
 import { Switch, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
@@ -6,12 +7,23 @@ import { Container } from 'react-bootstrap';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { createBrowserHistory } from 'history';
 import HomePage from './pages/homepage/homepage.component';
 import ContactPage from './pages/contact/contact.component';
 import ServicesPage from './pages/services/services.component';
 import ProjectsPage from './pages/projects/projects.component';
 import Header from './components/header/header.component';
 import Footer from './components/footer/footer.component';
+
+const TRACKING_ID = "UA-105251061-2";
+ReactGA.initialize(TRACKING_ID);
+
+const history = createBrowserHistory();
+
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 const particlesOptions = {
   particles: {
